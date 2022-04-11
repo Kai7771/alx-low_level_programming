875b51abb40e7697a606f75d96ff4445ce85db6d
Kai7771 <uzorc2@gmail.com>
2022-04-11 10:52:18 -0700

    low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,19 @@
 
 #include <stdio.h>
 /**
- * Main - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (success)
- *
 */
 int main(void)
 {
+	char ch = ' ';
+	int int_num = 0;
+	int long_int = 0l;
+	int long_long_int = 0ll;
+	float float_num = 0.0f;
+
+
 	printf("Size of char:%d\n", sizeof(char));
 	printf("Size of int:%d\n", sizeof(int));
 	printf("Size of a long int:%d\n", sizeof(long));
