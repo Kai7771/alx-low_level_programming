4aa83cdaa98810c78fadd20a32aac2da53da0961
Kai7771 <uzorc2@gmail.com>
2022-04-12 03:06:44 -0700

    low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,17 @@
 
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - This function prints the bytes of variables declared
  *
  * Return: Always 0 (success)
 */
 int main(void)
 {
 
-
-	printf("Size of char:%d\n", sizeof(char)) "byte (s)";
-	printf("Size of an int:%d\n", sizeof(int)) "byte(s)";
-	printf("Size of a long int:%d\n", sizeof(long)) "byte (s)";
-	printf("Size of a long long int:%d\n", sizeof(long long)) "byte (s)";
-	printf("Size of a float:%d\n", sizeof(float)) "byte (s)";
+	printf("Size of char:%d\n", sizeof(char)"byte(s)");
+	printf("Size of an int:%d\n", sizeof(int)"byte(s)");
+	printf("Size of a long int:%d\n", sizeof(long)"byte(s)");
+	printf("Size of a long long int:%d\n", sizeof(long long)"byte(s)";
+	printf("Size of a float:%d\n", sizeof(float)"byte(s)");
 	return (0);
 }
