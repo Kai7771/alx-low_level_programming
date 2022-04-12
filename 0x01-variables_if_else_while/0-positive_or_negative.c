e23728cf840a6dde357b39ed49bf37f6fc5a7cf3
Kai7771 <uzorc2@gmail.com>
2022-04-12 09:41:02 -0700

    checks the number position

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Assigns a random number
+ * prints if positive, negative or zero
+ *
+ * Return: return 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
+	else
+	{
+		printf("%i is zero\n");
+	}
+	return (0);
