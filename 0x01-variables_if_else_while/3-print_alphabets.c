6bce6cf0a823a7a2452a77f8023a6e2a6e9e8ddc
Kai7771 <uzorc2@gmail.com>
2022-04-13 11:35:26 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Letters of the alphabets are printed
+* in lower and upper cases.
+*
+* Return: programs returns 0 (Success)
+*/
+
+int main(void)
+{
+	char ch;
+
+	char CH;
+
+	ch = 'a';
+
+	ch = 'A';
+
+	do {
+		putchar(ch);
+		ch++;
+	} while (ch <= 'z');
+
+	do {
+		putchar(CH);
+		CH++;
+	} while (ch <= 'Z');
+
+	putchar('\n');
+
+	return (0);
+}
