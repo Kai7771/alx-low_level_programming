0ce12e81b4db616236baafd9734ea6dd87dba909
Kai7771 <uzorc2@gmail.com>
2022-09-09 06:49:03 -0700

    prints lower and upper cases

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,29 +1,22 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-
 /**
- * main - Entry point
+ * *main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-
 	char low;
-	char high;
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-	
-	for (high = 'A'; high <= 'Z'; high++)
-		putchar(high);
-		putchar('\n');
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+	putchar('\n');
 
-	return (0);
+	return(0);
 
-}
+}	
