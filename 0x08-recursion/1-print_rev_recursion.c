9730b1dac52399ef643cd22d30a8cbdd2bf2d4b7
Kai7771 <uzorc2@gmail.com>
2022-09-28 06:01:59 -0700

    recursive func that prints string in reserve

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+include "main.h"
+
+/**
+ * print_rev_recursion - prints string in reverse()
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+
+void _prints_rev_recursion(char *s)
+{
+	if (*s != '\0')
+		_print_rev_recursion( s + 1 );
+	else
+		return;
+	_putchar(*s);
+}
